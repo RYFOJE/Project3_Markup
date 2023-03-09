ab51dc7d55fdc053ff45e362ecf4c55a99a3d479
Ryan Jennings <ryfoje2@gmail.com>
2023-03-08 20:53:04 -0500

    Added code to count the amount of newlines after the paragraph

--- Project3_Markup/Source.cpp
+++ Project3_Markup/Source.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
+
+
 using namespace std;
+
+
+typedef struct textAndSpacing
+{
+	string text;
+	int spacing = 0;
+
+} textAndSpacing;
 
 /*
  * name: htmlHeader
@@ -50,18 +61,25 @@ string htmlFooter() {
  * 
  * returns: a string with the text surrounded by <p> and </p> tags
  */
-string surround_p(string text) {
+string surround_p(textAndSpacing textStruct) {
 
 	string formattedString;
 
     formattedString.append("<p>\n");
-	formattedString.append(text);
+	formattedString.append(textStruct.text);
 	formattedString.append("\n</p>\n");
     
+	for (size_t i = 0; i < textStruct.spacing; i++) {
+		formattedString.append("<br>\n");
+	}
+
 	return formattedString;
 
 }
 
+/*
+ *  
+*/
 string insert_br(string text) {
 
 	string formattedString = "<br>\n";
@@ -69,13 +87,87 @@ string insert_br(string text) {
 	return formattedString;
 }
 
+textAndSpacing get_text_spacing() {
+	
+	textAndSpacing textStruct;
+	string foundText;
+
+	// This will get all the text to be surrounded by <p>
+	while (true) {
+		string tempStr;
+
+		// If it's the end of file
+		/*if (cin.eof()) {
+			return textStruct;
+		}
+		*/
+		// Get the proceeding line
+		getline(cin, tempStr);
+
+		// TODO: Add code that will append the text to the textStruct.
+		// I need to take into account that the code will not include any newline characters
+
+
+		// If it was a new line
+		if (tempStr.empty()) {
+			break;
+		}
+
+	}
+
+	// This will get the total amount of blank lines after the end of the paragraph
+	while (true) {
+		char c = cin.peek();
+		//char c = cin.get();
+
+		if (c == '\n') {
+			cout << "Newline HIT" << endl;
+			textStruct.spacing++;
+		}
+
+		// If the next line is not a newline
+		if (c != '\n') {
+			if (c == EOF) {
+				cout << "EOF HIT" << endl;
+			}
+			break;
+		}
+
+		// Advance to what you've peeked (aka move to the next newline)
+		cout << (int)cin.get();
+	}
+
+	return textStruct;
+
+}
+
+bool fileExists(string fileName) {
+	std::ifstream infile(fileName.c_str());
+	return infile.good();
+}
+
 int main() {
     
-	string title;
+	string fileName = "test.txt";
 
-	cin >> title;
+	if (fileExists(fileName)) {
+		cout << "File exists" << endl;
+	}
 
-    cout << htmlHeader(title);
+	else {
+		cout << "File doesnt exist" << endl;
+		return 0;
+	}
+
+	// https://stackoverflow.com/questions/10150468/how-to-redirect-cin-and-cout-to-files
+	ifstream in(fileName);
+	cin.rdbuf(in.rdbuf()); //redirect std::cin to in.txt!
+
+	textAndSpacing text = get_text_spacing();
+
+	cout << text.text << endl;
+	cout << text.spacing << endl;
+
 
     return 0;
 }
