888a96922a90e77bcb154361e51d9db06091fef7
ryfoje <ryfoje2@gmail.com>
2023-03-09 22:06:47 -0500

    Added a testing branch. This will hold all testing features currently in development

--- testing/Source.cpp
+++ testing/Source.cpp
@@ -1,96 +1,114 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
-
-
+#include <vector>
 using namespace std;
 
+typedef struct cmd_struct {
 
+	bool isHelp = false;
+	bool isParagraph = false;
+	bool isReport = false;
 
-string print_whole_file() {
+	string inputFile = "";
+	string outputFile = "";
 
-	string tempStr;
+	bool isKeyword = false;
+	vector<string> keywords;
+	
+	bool notRecognized = false;
+	char notRecognizedChar = '0';
 
-	while (true) {
+} cmd_struct;
 
-		char c = cin.get();
-		tempStr.push_back(c);
-
-
-		if (cin.eof()) {
-			cout << "EOF\n";
-			return tempStr;
-		}
-
-		else if (c == '\n') {
-			cout << "\\n\n";
-		}
-
-		else {
-			cout << c;
-		}
-
+void print_cmd_struct(cmd_struct cmds) {
+	
+	cout << "isHelp: " << cmds.isHelp << endl;
+	cout << "isParagraph: " << cmds.isParagraph << endl;
+	cout << "isReport: " << cmds.isReport << endl;
+	cout << "inputFile: " << cmds.inputFile << endl;
+	cout << "outputFile: " << cmds.outputFile << endl;
+	cout << "isKeyword: " << cmds.isKeyword << endl;
+	cout << "keywords: ";
+	for (size_t i = 0; i < cmds.keywords.size(); i++) {
+		cout << ", " << cmds.keywords[i];
 	}
+
+	cout << endl;
+	cout << "notRecognized: " << cmds.notRecognized << endl;
+	cout << "notRecognizedChar: " << cmds.notRecognizedChar << endl;
+
+}
+
+cmd_struct find_args(int argc, char* argv[]) {
+
+	cmd_struct commands;
+	bool fileFound = false;
+
+	for (size_t i = 1; i < argc; i++) {
+		
+		string curr = argv[i];
+
+		if (commands.isKeyword) {
+			// Add the keyword to the vector
+			commands.keywords.push_back(curr);
+			continue;
+		}
+
+		// If any return command is found, simply end the search and return that is has been found
+		// this is because the program does not need to search for any other commands once --help is found
+		if (curr.compare("--help") == 0) {
+			commands.isHelp = true;
+			break;
+		}
+
+		else if (curr.compare("--keywords") == 0) {
+			commands.isKeyword = true;
+			continue;
+		}
+		
+		// If it's a switch
+		else if (curr[0] == '-') {
+			
+			for (size_t i = 1; i < curr.length(); i++) {
+				
+				if (curr[i] == 'p') {
+					commands.isParagraph = true;
+				}
+				else if (curr[i] == 'r') {
+					commands.isReport = true;
+				}
+
+				else {
+					commands.notRecognized = true;
+					commands.notRecognizedChar = curr[i];
+					return commands;
+				}
+				
+			}
+		}
+
+		// If it's a file
+		else if (curr.find(".") != string::npos) {
+			if (!fileFound) {
+				commands.inputFile = curr;
+				fileFound = true;
+			}
+			else {
+				commands.outputFile = curr;
+			}
+		}
+	}
+
+	return commands;
 
 }
 
 
-string surround(string& input, string search, string opening, string closing) {
+int main(int argc, char* argv[]) {
 
-	string tempStr;
-	size_t currOffset = 0;
-	size_t currIndex = 0;
+	cmd_struct cmds = find_args(argc, argv);
 
-	size_t searchWordLen = search.length();
-	size_t openingWordLen = opening.length();
-	size_t closingWordLen = closing.length();
-
-	while (true) {
-
-		size_t pos = input.find(search, currOffset);
-
-		if (pos == std::string::npos) {
-			std::cout << "Substring not found" << std::endl;
-			return input;
-		}
-		else {
-			currOffset = pos + searchWordLen;
-			std::cout << "Substring \"" << search <<"\" found at position " << pos << ". offsetting to: " << currOffset << endl;
-
-			input.insert(currOffset, closing);
-			input.insert(pos, opening);
-
-			currOffset += openingWordLen + closingWordLen;
-
-		}
-
-	}
-
-
-}
-
-
-int main() {
-
-	string fileName = "test.txt";
+	print_cmd_struct(cmds);
 	
-
-	// https://stackoverflow.com/questions/10150468/how-to-redirect-cin-and-cout-to-files
-	ifstream in(fileName);
-	cin.rdbuf(in.rdbuf()); //redirect
-
-	string text = print_whole_file();
-
-
-	string formattedText = surround(text, "red", "<open>", "<close>");
-
-	//cout << endl << endl;
-
-	
-
-	cout << text;
-
-
-	return 0;
 }
