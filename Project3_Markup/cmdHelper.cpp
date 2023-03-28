8248a65ec3d9d59b4f394454a93138b9b123f744
Ryan Jennings <ryfoje2@gmail.com>
2023-03-28 16:11:45 -0400

    Added documentation to cmdHelper

--- Project3_Markup/cmdHelper.cpp
+++ Project3_Markup/cmdHelper.cpp
@@ -3,6 +3,17 @@
 #include <iostream>
 #include "cmdHelper.h"
 
+/*
+ * Function:	parse_cmd
+ * purpose:		Parses the command line arguments and stores them in a struct
+ * 
+ * accepts:		int argc - the number of arguments passed to the program
+ *				char* argv[] - the arguments passed to the program
+ * 
+ * returns:		cmd_struct - a struct that holds the parsed values from the command line arguments
+ * 
+ * author: 		Ryan Jennings
+*/
 cmd_struct parse_cmd(int argc, char* argv[]) {
 
 	cmd_struct commands;
@@ -72,6 +83,14 @@ cmd_struct parse_cmd(int argc, char* argv[]) {
 
 }
 
+/*
+ * Function:	print_help
+ * purpose:		Prints the help text for the program when the --help or -h command is passed
+ *
+ * returns:		void
+ *
+ * author: 		Ryan Jennings
+*/
 void print_help() {
 	/*
 	markup v1.0.0, (c)2010 - 2023, Garth Santor
@@ -103,10 +122,30 @@ void print_help() {
 				
 }
 
+/*
+ * Function:	print_unrecognized
+ * purpose:		Shows to the user that a command line argument was not recognized and prints it
+ *
+ * accepts:		char arg - the argument that was not recognized
+ *
+ * returns:		void
+ *
+ * author: 		Ryan Jennings
+*/
 void print_unrecognized(char arg) {
 	std::cout << "Unrecognized argument: " << arg << std::endl;
 }
 
+/*
+ * Function:	print_file_not_found
+ * purpose:		Prints to the user that a file was not found
+ *
+ * accepts:		std::string filename - the filename that was not found
+ *
+ * returns:		void
+ *
+ * author: 		Ryan Jennings
+*/
 void print_file_not_found(std::string filename) {
 	
 	//TODO: Verify the project folder to make sure the output is correct
@@ -114,6 +153,18 @@ void print_file_not_found(std::string filename) {
 
 }
 
+/*
+ * Function:	find_output_file
+ * purpose:		Checks to see if the user passed a file output, if not it will generate a name
+ *
+ * accepts:		std::string inputFilename   -	the filename that was passed in
+ *				std::string &outputFilename -	the filename that will be generated or has
+ *												already been passed in		
+ *
+ * returns:		void
+ *
+ * author: 		Ryan Jennings
+*/
 void find_output_file(std::string inputFilename, std::string &outputFilename) {
 	
 	// If a output filename has already been found, return
